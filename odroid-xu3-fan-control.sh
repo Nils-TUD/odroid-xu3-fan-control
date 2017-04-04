23c5315883367a0e75dd8b8e45a60414882a6402
Nils Asmussen <nils@os.inf.tu-dresden.de>
2017-04-04 21:22:14 +0200

    Changes for Linux 4.9.

--- odroid-xu3-fan-control.sh
+++ odroid-xu3-fan-control.sh
@@ -15,49 +15,57 @@ if [ $EUID -ne 0 ]; then
    exit 1
 fi
 
-if [ -f /sys/devices/odroid_fan.13/fan_mode ]; then
-   FAN=13
-elif [ -f /sys/devices/odroid_fan.14/fan_mode ]; then
-   FAN=14
-else
+if [ ! -f /sys/class/thermal/cooling_device0/cur_state ]; then
    echo "This machine is not supported." >&2
    exit 1
 fi
 
-TEMPERATURE_FILE="/sys/devices/10060000.tmu/temp"
-FAN_MODE_FILE="/sys/devices/odroid_fan.$FAN/fan_mode"
-FAN_SPEED_FILE="/sys/devices/odroid_fan.$FAN/pwm_duty"
+set_mode() {
+	for i in /sys/class/thermal/thermal_zone?; do
+		echo $1 >$i/policy
+		echo $2 >$i/mode
+	done
+}
+enable_manual() {
+	set_mode user_space disabled
+}
+disable_manual() {
+	set_mode step_wise enabled
+}
+set_speed() {
+	echo $1 > /sys/class/thermal/cooling_device0/cur_state
+}
+
+get_temp() {
+	max=0
+	for i in /sys/class/thermal/thermal_zone?/temp; do
+		temp=$(cat $i)
+		if [ $temp -gt $max ]; then
+			max=$temp
+		fi
+	done
+	echo $max
+}
+
 TEST_EVERY=3 #seconds
 
 # Make sure after quiting script fan goes to auto control
 function cleanup {
   $DEBUG && echo "Fan control stopped (back to auto)." >&2
-  echo 1 > $FAN_MODE_FILE
+  disable_manual
 }
 trap cleanup EXIT
 
-function exit_xu3_only_supported {
-  $DEBUG && echo "Found non-xu3: $1" >&2
-  exit 2
-}
-if [ ! -f $TEMPERATURE_FILE ]; then
-  exit_xu3_only_supported "no temp file"
-elif [ ! -f $FAN_MODE_FILE ]; then
-  exit_xu3_only_supported "no fan mode file"
-elif [ ! -f $FAN_SPEED_FILE ]; then
-  exit_xu3_only_supported "no fan speed file"
-fi
-
 echo "Fan control started." >&2
 
-prev_fan_speed=0
 # To be sure we can manage fan
-echo 0 > $FAN_MODE_FILE
+enable_manual
 
 prev_temp=0
-while [ true ];
-do
-  current_max_temp=`cat $TEMPERATURE_FILE | cut -d: -f2 | sort -nr | head -1`
+prev_fan_speed=-1
+while [ true ]; do
+  current_max_temp=$(get_temp)
+  echo $current_max_temp
   if [ $current_max_temp -eq $prev_temp ]; then
     sleep $TEST_EVERY
     continue
@@ -65,27 +73,20 @@ do
 
   prev_temp=$current_max_temp
 
-  new_fan_speed=0
   if [ $current_max_temp -ge 75000 ]; then
-    new_fan_speed=255
-  elif [ $current_max_temp -ge 70000 ]; then
-    new_fan_speed=200
-  elif [ $current_max_temp -ge 68000 ]; then
-    new_fan_speed=130
-  elif [ $current_max_temp -ge 66000 ]; then
-    new_fan_speed=70
-  elif [ $current_max_temp -ge 63000 ]; then
-    new_fan_speed=65
+    new_fan_speed=3
   elif [ $current_max_temp -ge 60000 ]; then
-    new_fan_speed=60
+    new_fan_speed=2
+  elif [ $current_max_temp -ge 55000 ]; then
+    new_fan_speed=1
   else
-    new_fan_speed=50
+    new_fan_speed=0
   fi
 
   if [ $prev_fan_speed -ne $new_fan_speed ]; then
     $DEBUG && echo -n "Temp: $(($current_max_temp / 1000))C, " >&2
-    $DEBUG && echo "Fan speed: $(($new_fan_speed * 100 / 255))%" >&2
-    echo $new_fan_speed > $FAN_SPEED_FILE
+    $DEBUG && echo "Fan speed: $(($new_fan_speed * 100 / 3))%" >&2
+    set_speed $new_fan_speed
     prev_fan_speed=$new_fan_speed
   fi
 
